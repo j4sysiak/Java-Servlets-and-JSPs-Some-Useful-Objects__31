de210922607a48093718a3da7dd11211ea761a64
j4sysiak <j4sysiak@gmail.com>
2019-02-17 19:05:34 +0100

    1

---
+++ Java-Servlets-and-JSPs-Some-Useful-Objects__31/src/controller/Controller.java
@@ -0,0 +1,90 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Controller
+ */
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //Request scope
+		request.setAttribute("test", "Hello");
+		String test1 = (String)request.getAttribute("test");
+		
+		//Session scope
+		HttpSession session = request.getSession();
+		session.setAttribute("test", "Hello");
+		String test2 = (String)session.getAttribute("test");
+		
+		//Application scope
+		ServletContext context = getServletContext();
+		context.setAttribute("test", "Hello");
+		String test3 = (String)context.getAttribute("test");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
